ae2b3d67ddc642f38d43034d7d8d16fae60dd53d
Flavio Rogerio <solebac@hotmail.com>
2023-06-01 01:33:31 -0300

    Project Inatial

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>React Route Dom - Hooks</Text>
+    </SafeAreaView>
+  );
+}
